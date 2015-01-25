a8d55f7a029f6d286822961096a9dbfa6c4a915a
beng44 <beng38@outlook.com>
2015-01-25 12:22:45 -0700

    I changed the Coursera function to my own
    I also commented the code to describe what my program does.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This program receives a matrix, creates the inverse and caches it, then checks to see if the inverse has been taken and returns the inverse matrix.
 
-## Write a short comment describing this function
+## This function receives a matrix input, creates the inverse matrix, and caches the inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { #Only receives numeric inputs
+  m <- NULL #This block of code sets the value of the matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x #This gets the matrix
+  setinverse <- function(solve) m <<- solve #This solves the inverse for the matrix
+  getinverse <- function() m #Sets the value for the inverse of the matrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## This function retrieves the cached matrix, checks to see
+## if the matrix is an inverse, and then returns the 
+## inverse of the original matrix.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  m <- x$getinverse() #Retrieves the data from the first function
+  if(!is.null(m)) { #Checks to see if the data has already been manipulated into an inverse
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()#Solves for the inverse in the case that m has not been solved yet.
+  m <- solve(data,...)
+  x$setinverse(m)
+  m #returns m
 }
